b65ed1155fa070582068cece5893b4def26c65ab
Pawan Gautam <pawangtm2419@gmail.com>
2024-07-10 12:59:40 +0530

    uplkoad

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,5 @@
 function clickEvent() {
     document.getElementById("demo").innerHTML = "Hello Pawan";
     document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.fontSize = "xx-small";
 }
